bfb6ec008c5861e8f96f6b0ae469b5ce4bd7b687
Avaz <elonarm21@gmail.com>
2024-03-27 17:39:01 +0500

    contact-us component done

--- src/components/contact-us/ContactUs.jsx
+++ src/components/contact-us/ContactUs.jsx
@@ -1,17 +1,19 @@
- const ContactUs = () => {
+import "./ContactUs.css"
+
+const ContactUs = () => {
     return (
         <section className="contact-us">
             <div className="container">
                 <h2 className="title">Contact Us</h2>
                 <div className="contact-us__cards">
                     <div className="contact-us__card">
-                        <form action="">
-                            <h2>Send a message, ask a question</h2>
-                            <input type="text" placeholder="First name"/>
-                            <input type="number" placeholder="+99899 0000000"/>
-                            <input type="email" placeholder="E-mail"/>
-                            <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
-                            <button type="submit">Send</button>
+                        <form action="" className="contact-us__form">
+                            <h4>Send a message, ask a question</h4>
+                            <input type="text" placeholder="First name" id="name"/>
+                            <input type="number" placeholder="+99899 0000000" id="phoneNumber"/>
+                            <input type="email" placeholder="E-mail" id="email"/>
+                            <textarea name="textarea" id="textarea" cols="30" rows="10" placeholder="Message"></textarea>
+                            <button type="submit" className="contact-us__form-submit">Send</button>
                         </form>
                     </div>
                     <div className="contact-us__card">
