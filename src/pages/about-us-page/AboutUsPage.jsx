553afe9cf213402c0243a46ed6f21af0a241d9bb
Avaz <elonarm21@gmail.com>
2024-03-28 15:34:41 +0500

    about us page done

--- src/pages/about-us-page/AboutUsPage.jsx
+++ src/pages/about-us-page/AboutUsPage.jsx
@@ -1,62 +1,66 @@
+import "./AboutUsPage.css"
+
 const AboutUsPage = () => {
     return (
         <main className="main">
             <section className="about-us">
                 <div className="container">
-                    <h2>About us</h2>
-                    <h3>Capabilities and Power</h3>
-                    <p>
+                    <h2 className="title">About us</h2>
+                    <h3 className="about-us__title">Capabilities and Power</h3>
+                    <p className="about-us__description">
                         The DAKA-TEX factory is constantly working to expand its capabilities and increase capacity.
                         First of all, we are focused on the quality of our products, minimising costs, and fulfilling
                         orders on time.
                     </p>
-                    <h3>Production capacity</h3>
-                    <ul>
-                        <li>Spinning: 300 tons per month</li>
-                        <li>Knitting: 140 tons per month</li>
-                        <li>Dyeing: 300 tons per month</li>
-                        <li>Printing: 260 thousand units per month</li>
-                        <li>Sewing: 400 thousand items per month</li>
+                    <h3 className="about-us__title">Production capacity</h3>
+                    <ul className="about-us__list">
+                        <li className="about-us__item">Spinning: 300 tons per month</li>
+                        <li className="about-us__item">Knitting: 140 tons per month</li>
+                        <li className="about-us__item">Dyeing: 300 tons per month</li>
+                        <li className="about-us__item">Printing: 260 thousand units per month</li>
+                        <li className="about-us__item">Sewing: 400 thousand items per month</li>
                     </ul>
-                    <p>
+                    <p className="about-us__description">
                         And this is not the limit! The above capacities are achieved thanks to perfectly debugged
                         processes and advanced modern equipment.
                     </p>
-                    <h3>Equipment</h3>
-                    <p>
+                    <h3 className="about-us__title">Equipment</h3>
+                    <p className="about-us__description">
                         Modern fashion does not stand still. Designers regularly come up with incredible things, complex
                         mechanisms on products, accessories and much more. In turn, a modern factory should be ready for
                         such requests, which is why the DAKA-TEX company is equipped with modern equipment, including:
                     </p>
-                    <ul>
-                        <li>Equipment of a full cycle Reiter (Switzerland)</li>
-                        <li>Equipment of a full cycle Reiter (Switzerland)</li>
-                        <li>Equipment of a full cycle Reiter (Switzerland)</li>
-                        <li>Equipment of a full cycle Reiter (Switzerland)</li>
-                        <li>Equipment of a full cycle Reiter (Switzerland)</li>
-                        <li>Equipment of a full cycle Reiter (Switzerland)</li>
-                        <li>Equipment of a full cycle Reiter (Switzerland)</li>
-                        <li>Equipment of a full cycle Reiter (Switzerland)</li>
+                    <ul className="about-us__list">
+                        <li className="about-us__item">Equipment of a full cycle Reiter (Switzerland)</li>
+                        <li className="about-us__item">Equipment of a full cycle Reiter (Switzerland)</li>
+                        <li className="about-us__item">Equipment of a full cycle Reiter (Switzerland)</li>
+                        <li className="about-us__item">Equipment of a full cycle Reiter (Switzerland)</li>
+                        <li className="about-us__item">Equipment of a full cycle Reiter (Switzerland)</li>
+                        <li className="about-us__item">Equipment of a full cycle Reiter (Switzerland)</li>
+                        <li className="about-us__item">Equipment of a full cycle Reiter (Switzerland)</li>
+                        <li className="about-us__item">Equipment of a full cycle Reiter (Switzerland)</li>
                     </ul>
-                    <h3>DAKA-TEX: Textile company in Uzbekistan. Production and sale of textile products from Uzbek
-                        cotton</h3>
-                    <p>
-                        "DAKA-TEX" was established in early 2008 in Samarkand (Republic of Uzbekistan).
+                    <h3 className="about-us__title">DAKA-TEX: Textile company in Uzbekistan. Production and sale of
+                        textile products from Uzbek
+                        cotton
+                    </h3>
+                    <p className="about-us__description">
+                        DAKA-TEX was established in early 2008 in Samarkand (Republic of Uzbekistan).
                     </p>
-                    <p>
+                    <p className="about-us__description">
                         Today DAKA-TEX is one of the most modern and technologically equipped vertically integrated
                         factories in Uzbekistan.
                     </p>
-                    <h3>
+                    <h3 className="about-us__title">
                         DAKA-TEX production includes:
                     </h3>
-                    <ul>
-                        <li>Cotton yarn;</li>
-                        <li>Cotton yarn;</li>
-                        <li>Cotton yarn;</li>
-                        <li>Cotton yarn;</li>
+                    <ul className="about-us__list">
+                        <li className="about-us__item">Cotton yarn;</li>
+                        <li className="about-us__item">Cotton yarn;</li>
+                        <li className="about-us__item">Cotton yarn;</li>
+                        <li className="about-us__item">Cotton yarn;</li>
                     </ul>
-                    <p>
+                    <p className="about-us__description">
                         The complex of construction, installation and commissioning works for the spinning mill was
                         completed by the end of 2009. In 2014, a sewing and knitwear production was launched on 5
                         production lines. In 2018, we expanded the production area of the garment production and
@@ -64,49 +68,58 @@ const AboutUsPage = () => {
                         cutting and printing equipment. The production of knitted fabric was launched in 2019, a new
                         production building was built at 10 sqm and installed the most modern equipment.
                     </p>
-                    <h3>
+                    <h3 className="about-us__title">
                         Company`s mission
                     </h3>
-                    <p>
+                    <p className="about-us__description">
                         The company`s mission is to produce high quality yarns, knitted fabrics and finished knitwear
                         based on the following features:
                     </p>
-                    <ul>
-                        <li>our quality is at the level of world standards, because we control all stages of production,
+                    <ul className="about-us__list">
+                        <li className="about-us__item">
+                            our quality is at the level of world standards, because we control all stages of production,
                             from yarn to finished knitwear;
                         </li>
-                        <li>our quality is at the level of world standards, because we control all stages of production,
+                        <li className="about-us__item">
+                            our quality is at the level of world standards, because we control all stages of production,
                             from yarn to finished knitwear;
                         </li>
-                        <li>our quality is at the level of world standards, because we control all stages of production,
+                        <li className="about-us__item">
+                            our quality is at the level of world standards, because we control all stages of production,
                             from yarn to finished knitwear;
                         </li>
                     </ul>
-                    <h3>
+                    <h3 className="about-us__title">
                         Advantages
                     </h3>
-                    <ul>
-                        <li>DAKA-TEX is a vertically integrated organisation that includes all stages: from the
+                    <ul className="about-us__list">
+                        <li className="about-us__item">
+                            DAKA-TEX is a vertically integrated organisation that includes all stages: from the
                             production of cotton yarn to the packaging of finished knitwear. This is the reason for the
                             competitive price and high quality.
                         </li>
-                        <li>DAKA-TEX is a vertically integrated organisation that includes all stages: from the
+                        <li className="about-us__item">
+                            DAKA-TEX is a vertically integrated organisation that includes all stages: from the
                             production of cotton yarn to the packaging of finished knitwear. This is the reason for the
                             competitive price and high quality.
                         </li>
-                        <li>DAKA-TEX is a vertically integrated organisation that includes all stages: from the
+                        <li className="about-us__item">
+                            DAKA-TEX is a vertically integrated organisation that includes all stages: from the
                             production of cotton yarn to the packaging of finished knitwear. This is the reason for the
                             competitive price and high quality.
                         </li>
-                        <li>DAKA-TEX is a vertically integrated organisation that includes all stages: from the
+                        <li className="about-us__item">
+                            DAKA-TEX is a vertically integrated organisation that includes all stages: from the
                             production of cotton yarn to the packaging of finished knitwear. This is the reason for the
                             competitive price and high quality.
                         </li>
-                        <li>DAKA-TEX is a vertically integrated organisation that includes all stages: from the
+                        <li className="about-us__item">
+                            DAKA-TEX is a vertically integrated organisation that includes all stages: from the
                             production of cotton yarn to the packaging of finished knitwear. This is the reason for the
                             competitive price and high quality.
                         </li>
-                        <li>DAKA-TEX is a vertically integrated organisation that includes all stages: from the
+                        <li className="about-us__item">
+                            DAKA-TEX is a vertically integrated organisation that includes all stages: from the
                             production of cotton yarn to the packaging of finished knitwear. This is the reason for the
                             competitive price and high quality.
                         </li>
