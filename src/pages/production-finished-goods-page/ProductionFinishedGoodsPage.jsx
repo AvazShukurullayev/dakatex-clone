458f6fbea4d23bbbb9d90765b888ba8c77dba7a5
Avaz <elonarm21@gmail.com>
2024-03-28 15:59:29 +0500

    production finishedgoods

--- src/pages/production-finished-goods-page/ProductionFinishedGoodsPage.jsx
+++ src/pages/production-finished-goods-page/ProductionFinishedGoodsPage.jsx
@@ -1,9 +1,11 @@
+import "./ProductionFinishedGoodsPage.css"
+
 export const ProductionFinishedGoodsPage = () => {
     return (
         <main className="main">
             <section className="finished-goods">
                 <div className="container">
-                    <h2>Finished goods</h2>
+                    <h2 className="title">Finished goods</h2>
                     <div className="finished-goods__bg"></div>
                 </div>
             </section>
